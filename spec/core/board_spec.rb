59eae5dbf2a07b25c47766053d621b405b8e6116
Jesus Millan <jgmc3012@gmail.com>
2021-09-19 23:01:41 -0400

    recycle: Change file names

--- spec/core/board_spec.rb
+++ spec/core/board_spec.rb
@@ -2,20 +2,33 @@ require 'rails_helper'
 require_relative '../../app/core'
 
 describe 'Board' do
-  describe 'star a game' do
-    describe 'as new game' do
-      game = Core::Game.new
-      mines = 10
-      game.new_game(width: 10, heigth: 10, mines: mines)
-      it 'The user board should be hide' do
-        expect(game.user_board.all?(Core::Cells::HIDE)).to eq(true)
-      end
-      it 'Total mines should be indicated' do
-        expect(game.mines_board.count(Core::Cells::MINE)).to eq(mines)
-      end
-      it 'The flag board should be void' do
-        expect(game.flags_board.all?(Core::Cells::VOID)).to eq(true)
-      end
+  describe 'cell is null?' do
+    it 'should return true if cell is null' do
+      board = Core::Board.new(2, 2, Core::Cells::VOID)
+      expect(board.cell_is_void?(0, 0)).to eq(true)
+    end
+    it 'should return false if cell is not null' do
+      board = Core::Board.new(2, 2, Core::Cells::VOID)
+      board[0, 0] = 'X'
+      expect(board.cell_is_void?(0, 0)).to eq(false)
+    end
+  end
+
+  describe 'toggle flag' do
+    it 'active flag if cell is null' do
+      board = Core::Board.new(2, 2, Core::Cells::VOID)
+      board.toggle_flag(x: 0, y: 0, flag: Core::Cells::QUESTION_FLAG)
+      expect(board[0, 0]).to eq(Core::Cells::QUESTION_FLAG)
+    end
+    it 'desactive flag if cell is equal to flag' do
+      board = Core::Board.new(2, 2, Core::Cells::QUESTION_FLAG)
+      board.toggle_flag(x: 0, y: 0, flag: Core::Cells::QUESTION_FLAG)
+      expect(board[0, 0]).to eq(Core::Cells::VOID)
+    end
+    it 'active flag if cell is equal to other flag' do
+      board = Core::Board.new(2, 2, Core::Cells::QUESTION_FLAG)
+      board.toggle_flag(x: 0, y: 0, flag: Core::Cells::RED_FLAG)
+      expect(board[0, 0]).to eq(Core::Cells::RED_FLAG)
     end
   end
 end
