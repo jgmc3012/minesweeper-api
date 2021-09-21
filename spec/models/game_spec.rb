8d0792f7706f2593524cda798d2d266abde6a925
Jesus Millan <jgmc3012@gmail.com>
2021-09-20 22:09:33 -0400

    feat: Test for models

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'valdiations model' do
+      it 'validations for required field' do
+        should validate_presence_of(:user_id)
+        should validate_presence_of(:mines_board)
+        should validate_presence_of(:user_board)
+        should validate_inclusion_of(:is_over).in_array([true, false])
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'valdiations model' do
+    it 'validations for required field' do
+      should validate_presence_of(:username)
+    end
+  end
 end
