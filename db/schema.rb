111842a24c9ea1a3537c017e39d038039e6fcd9b
Jesus Millan <jgmc3012@gmail.com>
2021-09-21 04:53:09 -0400

    feat: Migrate DB to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_20_090022) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.boolean "is_over"
     t.json "user_board"
--- docker/local/rails/entrypoint.sh
+++ docker/local/rails/entrypoint.sh
@@ -6,4 +6,7 @@ if [ -f /minesweeper/tmp/pids/server.pid ]; then
     rm /minesweeper/tmp/pids/server.pid
 fi
 
+rake db:setup
+rake db:migrate
+
 bundle exec rails s -b 0.0.0.0
